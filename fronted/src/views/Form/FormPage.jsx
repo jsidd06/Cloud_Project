9ff32c72bf1e8a4d1b0ad6ce293b67986ebe5380
siddharth <jsidd06@gmail.com>
2022-01-19 21:23:30 +0530

    map the formfield

--- fronted/src/views/Form/FormPage.jsx
+++ fronted/src/views/Form/FormPage.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Container, Row, Col, Card, CardBody, Input } from 'reactstrap'
+import { formFields } from '../../fake-data'
 
 function FormPage() {
-  return <div>
-    
-  </div>
+  return (
+    <Container>
+      <Row>
+        <Col sm={12} md={6}></Col>
+        <Col sm={12} md={6}>
+          {formFields.map((field, index) => (
+            <Card key={index} className="m-2 p-4">
+              <CardBody>
+                <Row>
+                  <Col>
+                    <p style={{ fontSize: 20 }}>{field.label}</p>
+                  </Col>
+                  <Col>
+                    <Input type="checkbox" style={{ fontSize: 30 }} />
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          ))}
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default FormPage
