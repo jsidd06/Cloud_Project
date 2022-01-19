c7d3f96ab28a1c6a58604aa695ec0b09e9b758e3
siddharth <jsidd06@gmail.com>
2022-01-19 12:45:02 +0530

    add search option with using filter

--- fronted/src/views/Home/Home.jsx
+++ fronted/src/views/Home/Home.jsx
@@ -3,22 +3,34 @@ import { useState } from 'react'
 import { Container, Input } from 'reactstrap'
 import data from '../../Components/data/Data'
 function Home() {
-  const [search, setSearch] = useState("")
+  const [searchTerm, setSearchTerm] = useState('')
   return (
     <Container>
       <Input
-        placeholder="Search"
+        placeholder="Search.."
         type="text"
-        onChange={(event) => console.log(setSearch(event.target.value))}
+        onChange={(event) => console.log(setSearchTerm(event.target.value))}
       />
-
-      {data.map((item, index) =>  { return (
-        <div key={index}>
-          <h1>{item.name}</h1>
-          <p>{item.age}</p>
-        </div>
-      ) 
-      })}
+      {data
+        .filter((item) => {
+          if (searchTerm === '') {
+            return item
+          } else if (
+            item.name
+              .toLocaleLowerCase()
+              .includes(searchTerm.toLocaleLowerCase())
+          ) {
+            return item
+          } 
+        })
+        .map((item, index) => {
+          return (
+            <div key={index}>
+              <h1>{item.name}</h1>
+              <p>{item.age}</p>
+            </div>
+          )
+        })}
     </Container>
   )
 }
