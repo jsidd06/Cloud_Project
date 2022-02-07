2ba6497ca737f57978170ed595ae53040b37e109
siddharth <jsidd06@gmail.com>
2022-02-07 19:02:01 +0530

    check the user id for login to signup with mongoose

--- Backend/index.js
+++ Backend/index.js
@@ -1,60 +1,69 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true }).then((req) => {
-    console.log('f*ck to MongoDB');
-}).catch((err) => {
-    console.log('Error: ', err);
-})
-
-// set the schema for mongoose
-const newSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-})
-
-const loginUser = mongoose.model('loginUser', newSchema);
-
-app.post('/login', (req, res) => {
-    new loginUser ({ username: req.body.username, password: req.body.password }).save(function(err){
-        if(!err){
-            res.send('Successfully login');
-        }else{
-            res.send("error login");
-        }
-    })
-})
+mongoose
+  .connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((req) => {
+    console.log("f*ck to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
 
 // signup to MongoDB
 const newSchema2 = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    username: String,
-    password: String,
-})
+  firstName: String,
+  lastName: String,
+  username: String,
+  password: String,
+});
 
-const signUpUser = mongoose.model("signUpUser", newSchema2);
+const User = mongoose.model("User", newSchema2);
 
-app.post('/signup', (req, res) => {
-    new signUpUser ({ firstName: req.body.firstName, lastName: req.body.lastName, username: req.body.username, password: req.body.password }).save(function(err){
-        if(!err){
-            res.send('Successfully signup');
-        }else{
-            res.send("error signup");
-        }
-    })
-})
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  User.findOne({ username }, (err, data) => {
+    if (err) {
+      res.send(err);
+    } else if (data) {
+      if (data.password === password) {
+        res.send(data);
+      } else {
+        res.send("wrong password");
+      }
+    } else {
+      res.send("user not found");
+    }
+  });
+});
 
+app.post("/signup", (req, res) => {
+  new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    username: req.body.username,
+    password: req.body.password,
+  }).save(function (err) {
+    if (!err) {
+      res.send("Successfully signup");
+    } else {
+      res.send("error signup");
+    }
+  });
+});
 
 app.listen(5000, () => {
-    console.log('future f*ck is running on Port 5000')
-})
+  console.log("future f*ck is running on Port 5000");
+});
