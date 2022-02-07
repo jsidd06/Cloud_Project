418122df4e9915906f702aac8c81fde4c7529710
siddharth <jsidd06@gmail.com>
2022-02-07 17:55:28 +0530

    connect with mongodb

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,20 @@
-const express = require('express');
+import dotenv from "dotenv";
+dotenv.config();
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
 const app = express();
 
+app.use(cors());
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('hello world')
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true }).then((req) => {
+    console.log('f*ck to MongoDB');
+}).catch((err) => {
+    console.log('Error: ', err);
 })
-
-
 
 app.listen(5000, () => {
     console.log('future f*ck is running on Port 5000')
