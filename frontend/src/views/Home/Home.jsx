a15ddf44a49373b6446b268e79c8b43d7999e6ac
siddharth <jsidd06@gmail.com>
2022-02-09 14:31:19 +0530

    add paragraph

--- frontend/src/views/Home/Home.jsx
+++ frontend/src/views/Home/Home.jsx
@@ -10,7 +10,20 @@ function Home() {
         <Link className="btn btn-warning mt-5" to="/form">
           Click here to Start
         </Link>
-        <video muted loop autoPlay preload="auto" style={{maxWidth: '100%',marginTop: '10px'}}  src={Video}></video>
+        <video
+          muted
+          loop
+          autoPlay
+          preload="auto"
+          style={{ maxWidth: '100%', marginTop: '10px' }}
+          src={Video}
+        ></video>
+      </div>
+      <div style={{ textAlign: 'center', marginTop: '10px' }}>
+        <p>
+          Developed by AKSHAY KUMAR SINGH & SIDDHARTH JAIN
+          <p>Copyright &copy;2022</p>
+        </p>
       </div>
     </Layout>
   )
