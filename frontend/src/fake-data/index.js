2daa497efb376f4e9b46d36081667c0c1bfe0e3c
siddharth <jsidd06@gmail.com>
2022-02-09 12:47:24 +0530

    fixed the fake data name entry

--- frontend/src/fake-data/index.js
+++ frontend/src/fake-data/index.js
@@ -6,7 +6,7 @@ export const formFields = [
     label: 'User Full Name',
   },
   {
-    id: 16,
+    id: 26,
     type: 'number',
     name: 'gst',
     label: 'GST Number',
@@ -32,13 +32,13 @@ export const formFields = [
   {
     id: 3,
     type: 'number',
-    name: 'order number of quantity',
+    name: 'order_number_of_quantity',
     label: 'Order Number of Quantity',
   },
   {
     id: 4,
     type: 'textarea',
-    name: 'extra details',
+    name: 'extra_details',
     label: 'Extra Details',
   },
   {
@@ -50,47 +50,47 @@ export const formFields = [
   {
     id: 6,
     type: 'number',
-    name: 'number of pricing list',
+    name: 'number_of_pricing_list',
     label: 'Number of Pricing List',
   },
   {
     id: 7,
     type: 'number',
-    name: 'number of received order',
+    name: 'number_of_received_order',
     label: 'Number of Received Order',
   },
   {
     id: 8,
     type: 'number',
-    name: 'number of order',
+    name: 'number_of_order',
     label: 'Number of Order',
   },
   {
     id: 9,
     type: 'number',
-    name: 'number of holding payment',
+    name: 'number_of_holding_payment',
     label: 'Number of Holding Payment',
   },
   {
     id: 10,
     type: 'number',
-    name: 'number of received check',
+    name: 'number_of_received_check',
     label: 'Number of Received Check',
   },
   {
     id: 11,
     type: 'number',
-    name: 'number of holding order',
+    name: 'number_of_holding_order',
     label: 'Number of Holding Order',
   },{
     id: 12,
     type: 'number',
-    name: 'number of confirm payment',
+    name: 'number_of_confirm_payment',
     label: 'Number of Confirm Payment',
   },{
     id: 13,
     type: 'number',
-    name: 'number of confirm order',
+    name: 'number_of_confirm_order',
     label: 'Number of Confirm Order',
   },{
     id: 16,
@@ -143,5 +143,155 @@ export const formFields = [
     type: 'date',
     name: 'date',
     label: 'Date',
+  },{
+    id: 27,
+    type: 'number',
+    name: 'invoice_number',
+    label: 'Invoice Number',
+  },{
+    id: 28,
+    type: 'number',
+    name: 'invoice_amount',
+    label: 'Invoice Amount',
+  },{
+    id: 29,
+    type: 'date',
+    name: 'invoice_due_date',
+    label: 'Invoice Due Date',
+  },{
+    id: 30,
+    type: 'number',
+    name: 'invoice_due_amount',
+    label: 'Invoice Due Amount',
+  },{
+    id: 31,
+    type: 'number',
+    name: 'invoice_paid_amount',
+    label: 'Invoice Paid Amount',
+  },{
+    id: 32,
+    type: 'date',
+    name: 'invoice_paid_date',
+    label: 'Invoice Paid Date',
+  },{
+    id: 33,
+    type: 'text',
+    name: 'invoice_status',
+    label: 'Invoice Status',
+  },{
+    id: 34,
+    type: 'text',
+    name: 'invoice_payment_method',
+    label: 'Invoice Payment Method',
+  },{
+    id: 35,
+    type: 'text',
+    name: 'invoice_payment_status',
+    label: 'Invoice Payment Status',
+  },{
+    id: 36,
+    type: 'date',
+    name: 'invoice_payment_date',
+    label: 'Invoice Payment Date',
+  },{
+    id: 37,
+    type: 'number',
+    name: 'invoice_payment_amount',
+    label: 'Invoice Payment Amount',
+  },{
+    id: 38,
+    type: 'number',
+    name: 'invoice_payment_pin_code',
+    label: 'Invoice Payment Pin Code',
+  },{
+    id: 39,
+    type: 'text',
+    name: 'invoice_payment_address',
+    label: 'Invoice Payment Address',
+  },{
+    id: 40,
+    type: 'text',
+    name: 'invoice_payment_city',
+    label: 'Invoice Payment City',
+  },{
+    id: 41,
+    type: 'text',
+    name: 'invoice_payment_state',
+    label: 'Invoice Payment State',
+  },{
+    id: 42,
+    type: 'text',
+    name: 'invoice_payment_country',
+    label: 'Invoice Payment Country',
+  },{
+    id: 43,
+    type: 'number',
+    name: 'invoice_payment_zip_code',
+    label: 'Invoice Payment Zip Code',
+  },{
+    id: 44,
+    type: 'text',
+    name: 'invoice_payment_payment_method',
+    label: 'Invoice Payment Payment Method',
+  },{
+    id: 45,
+    type: 'text',
+    name: 'invoice_payment_payment_status',
+    label: 'Invoice Payment Payment Status',
+  },{
+    id: 46,
+    type: 'date',
+    name: 'invoice_payment_payment_date',
+    label: 'Invoice Payment Payment Date',
+  },{
+    id: 47,
+    type: 'number',
+    name: 'invoice_payment_payment_amount',
+    label: 'Invoice Payment Payment Amount',
+  },{
+    id: 48,
+    type: 'number',
+    name: 'invoice_payment_payment_pin_code',
+    label: 'Invoice Payment Payment Pin Code',
+  },{
+    id: 49,
+    type: 'text',
+    name: 'invoice_payment_payment_address',
+    label: 'Invoice Payment Payment Address',
+  },{
+    id: 50,
+    type: 'text',
+    name: "bank_account_name",
+    label: 'Bank Account Name',
+  },{
+    id: 51,
+    type: 'number',
+    name: "upi_transaction_id",
+    label: 'UPI Transaction ID',
+  },{
+    id: 52,
+    type: 'text',
+    name: "google_transaction_id",
+    label: 'Google Transaction ID',
+  },{
+    id: 53,
+    type: 'text',
+    name: "paytm_transaction_id",
+    label: 'PaYtm Transaction ID',
+  },{
+    id: 54,
+    type: 'text',
+    name: "paypal_transaction_id",
+    label: 'Paypal Transaction ID',
+  },{
+    id: 55,
+    type: 'text',
+    name: "bank_transaction_id",
+    label: 'Bank Transaction ID',
+  },{
+    id: 56,
+    type: 'text',
+    name: "phonepay_transaction_id",
+    label: 'PhonePay Transaction ID',
   }
 ]
